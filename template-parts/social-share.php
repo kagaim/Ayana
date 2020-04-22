5940d829968f3620fc03eec5266c3ac7fb6be4cb
kagaim <ekagai@gmail.com>
2020-04-22 18:30:55 +0300

    removed google plus social share link

--- template-parts/social-share.php
+++ template-parts/social-share.php
@@ -12,8 +12,6 @@
     <a href="https://twitter.com/share?text=<?php echo urlencode(get_the_title()); ?>&amp;url=<?php the_permalink(); ?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;"><i class="fa fa-twitter"></i></a>
     <!-- facebook -->
     <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;"><i class="fa fa-facebook"></i></a>
-    <!-- google plus -->
-    <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;"><i class="fa fa-google-plus"></i></a>
     <!-- linkedin -->
     <a href="https://www.linkedin.com/shareArticle?mini=true%26url=<?php the_permalink(); ?>%26source=<?php home_url(); ?>" onclick="window.open(this.href, 'linkedin-share', 'width=490,height=530');return false;"><i class="fa fa-linkedin"></i></a>
     <!-- pinterest -->
